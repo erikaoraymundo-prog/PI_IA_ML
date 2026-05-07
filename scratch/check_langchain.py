e333679cb20749160933b77e98d27d2612f07a59
erikaoraymundo-prog <erikao.raymundo@gmail.com>
2026-05-07 10:57:40 -0300

    .

--- scratch/check_langchain.py
+++
@@ -1,5 +0,0 @@
-import langchain.agents as ag
-all_items = dir(ag)
-print("ALL in langchain.agents:")
-for item in all_items:
-    print(" -", item)
--- scratch/test_agent_import.py
+++
@@ -1,2 +0,0 @@
-from backend.agente_recruter.main import run_background_check
-print("Import OK!")
